b1842263cb6ab1782e9fe0e63c13be90b07f3191
Zaid Khan <160527912+zaidkhan06@users.noreply.github.com>
2026-03-01 20:49:03 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,58 +17,26 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-
-// Body parser
 app.use(express.json());
-
-// Cookie parser
 app.use(cookieParser());
-
-// CORS 
-const allowedOrigins = [
-  "http://localhost:5173",  // Local dev        
-  process.env.CLIENT_URL  // Main production domain
-];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (
-        !origin ||         // Allow non-browser tools
-        allowedOrigins.includes(origin) ||     // Exact matches
-        origin.includes("vercel.app")          // Allow Vercel preview URLs
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
+    origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
-
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/coding", codingRoutes);
 app.use("/api/aptitude", aptitudeRoutes);
 app.use("/api/interview", interviewExprience);
-
 app.use("/api/payment", paymentRoutes);
 
-
-//db
 databaseConnection();
-
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// app.listen(5000, "0.0.0.0", () => {
-//   console.log("Server running on port 5000");
-// });
